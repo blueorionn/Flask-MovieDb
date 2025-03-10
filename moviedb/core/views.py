42cf1d27a67ffca131fc3abe291490450cc4bd7e
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-10 19:56:53 +0530

    added logic for data fetching, serving movie posters

--- moviedb/core/views.py
+++ moviedb/core/views.py
@@ -1,6 +1,7 @@
 """Core views."""
 
-from flask import Blueprint, render_template
+import os
+from flask import current_app, Blueprint, render_template, send_file
 from flask.views import MethodView
 from .func import list_movies
 
@@ -9,8 +10,16 @@ blueprint = Blueprint("core", __name__)
 
 class IndexView(MethodView):
     def get(self):
-        print(list_movies())
-        return render_template("index.html")
+        movies = list_movies()
+        context = {"movies": movies}
+        return render_template("index.html", **context)
+
+
+@blueprint.route("/movie/poster/<filename>")
+def serve_poster(filename):
+    path = os.path.join(current_app.config["APP_DIR"], f"assets/{filename}")
+
+    return send_file(path, mimetype="image/jpg")
 
 
 index_view = IndexView.as_view("home")
