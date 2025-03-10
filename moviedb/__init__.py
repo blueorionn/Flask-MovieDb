5eeedbe6c8cabda692987a08ab9241a2c2e81ab2
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-10 13:59:28 +0530

    Initial configuration

---
+++ moviedb/__init__.py
@@ -0,0 +1,58 @@
+"""Main application package."""
+
+from flask import Flask, render_template
+
+from moviedb.settings import config
+from moviedb.extensions import init_cors
+from moviedb import core, auth
+
+
+def create_app(config_object=config):
+    """Create an application factory
+
+    :param config_object: The configuration object to use
+    """
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+
+    # log config_object type
+    app.logger.info(f"Using {config_object.__class__.__name__}")
+    app.logger.info(f"Debug mode is {config_object.DEBUG}")
+
+    register_extension(app)
+    register_blueprints(app)
+    register_error_handlers(app)
+
+    return app
+
+
+def register_extension(app: Flask):
+    """Registering extensions."""
+
+    init_cors(app)
+
+
+def register_blueprints(app: Flask):
+    """Registering blueprints."""
+
+    app.register_blueprint(core.views.blueprint)
+    app.register_blueprint(auth.views.blueprint)
+
+
+def register_error_handlers(app: Flask):
+    """Registering error handlers."""
+
+    @app.errorhandler(404)
+    def not_found(e):
+        data = {"error_code": "404", "error_message": "Page Not Found"}
+        return render_template("handlers/handler.html", **data), 404
+
+    @app.errorhandler(405)
+    def method_not_allowed(e):
+        data = {"error_code": "405", "error_message": "Method Not Allowed"}
+        return render_template("handlers/handler.html", **data), 405
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        data = {"error_code": "500", "error_message": "Internal Server Error"}
+        return render_template("handlers/handler.html", **data), 500
---
+++ moviedb/settings.py
@@ -0,0 +1,49 @@
+"""Application Configuration."""
+
+import os
+from .utils import generate_secret_key
+
+
+class Config:
+    """Base Configuration."""
+
+    SECRET_KEY = os.environ.get("SECRET_KEY", generate_secret_key())
+    APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
+    PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
+    # File size restriction
+    MAX_CONTENT_LENGTH = 24 * 1024 * 1024  # 24 megabytes
+    # Database variable
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_PORT = os.environ.get("DB_PORT")
+
+
+class DevelopmentConfig(Config):
+    """Development Configuration."""
+
+    ENV = "dev"
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Production Configuration."""
+
+    ENV = "prod"
+    DEBUG = False
+
+
+class TestingConfig(Config):
+    """Testing Configuration."""
+
+    TESTING = True
+    DEBUG = True
+
+
+if os.environ.get("FLASK_ENV") == "development":
+    config = DevelopmentConfig()
+elif os.environ.get("FLASK_ENV") == "testing":
+    config = TestingConfig()
+else:
+    config = ProductionConfig()
