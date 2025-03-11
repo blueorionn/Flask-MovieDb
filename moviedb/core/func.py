868223ee82338b4db436e43246b7512a2f5b29e3
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-11 13:24:51 +0530

    added update movie ui

--- moviedb/core/func.py
+++ moviedb/core/func.py
@@ -21,3 +21,20 @@ def list_movies():
     movies = list(movie_collection.find({}))
 
     return movies
+
+def get_movie(id: str):
+    """Get Movie by Id"""
+
+    # creating pymongo client
+    mongo_client = pymongo.MongoClient(os.environ.get("MONGO_URI"))
+
+    # movies database
+    movies_db = mongo_client["movies"]
+
+    # movie collection
+    movie_collection: Collection = movies_db["movie"]
+
+    # getting movies
+    movie = movie_collection.find_one({"id": id})
+
+    return movie
--- moviedb/core/views.py
+++ moviedb/core/views.py
@@ -1,9 +1,9 @@
 """Core views."""
 
 import os
-from flask import current_app, Blueprint, render_template, send_file
+from flask import current_app, request, Blueprint, render_template, send_file
 from flask.views import MethodView
-from .func import list_movies
+from .func import list_movies, get_movie
 
 blueprint = Blueprint("core", __name__)
 
@@ -22,5 +22,16 @@ def serve_poster(filename):
     return send_file(path, mimetype="image/jpg")
 
 
+class UpdateMovie(MethodView):
+    def get(self, movie_id):
+        # get movie
+        movie = get_movie(str(movie_id))
+        context = {"movie": movie}
+        return render_template("movie.html", **context)
+
+
 index_view = IndexView.as_view("home")
 blueprint.add_url_rule("/", view_func=index_view)
+
+update_view = UpdateMovie.as_view("update_movie")
+blueprint.add_url_rule("/movie/<uuid:movie_id>", view_func=update_view)
