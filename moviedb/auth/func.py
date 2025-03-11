aaad3da42cbcb9a6c0407e15a9797b0588618bdd
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-11 21:15:32 +0530

    added authenticate_user function

---
+++ moviedb/auth/func.py
@@ -0,0 +1,39 @@
+import os
+import bcrypt
+import pymongo
+from datetime import timedelta
+from moviedb.utils import is_valid_uuid_v4
+from pymongo.collection import Collection
+
+
+def authenticate_user(username: str, password: str):
+    """Check if user exists with valid credentials."""
+
+    # creating pymongo client
+    mongo_client = pymongo.MongoClient(os.environ.get("MONGO_URI"))
+
+    # auth database
+    auth_db = mongo_client["auth"]
+
+    # user collection
+    user_collection: Collection = auth_db["user"]
+
+    # getting user
+    user = user_collection.find_one({"username": username})
+
+    # If user exist
+    if user is not None or (not isinstance(user, dict)):
+        # Checking if password is correct
+        fetched_user_id, fetched_username = (user["id"], user["username"])
+        fetched_password: str = user["password"]
+
+        if (
+            (bcrypt.checkpw(password.encode("utf-8"), fetched_password.encode("utf-8")))
+            and (username == fetched_username)
+            and is_valid_uuid_v4(fetched_user_id)
+        ):
+            return True
+        else:
+            return False
+    else:
+        return False
--- moviedb/auth/views.py
+++ moviedb/auth/views.py
@@ -1,13 +1,20 @@
 """Auth views."""
 
-from flask import Blueprint, render_template
+from flask import request, Blueprint, render_template
 from flask.views import MethodView
+from .func import authenticate_user
 
 blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 class LoginView(MethodView):
     def get(self):
+        return render_template("login.html")
+
+    def post(self):
+        username = request.form.get("username")
+        password = request.form.get("password")
+
         return render_template("login.html")
 
 
